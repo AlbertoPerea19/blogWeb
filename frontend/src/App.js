b22fd35188d3073dd134dc07ad94ff4c173df30a
AlbertoPerea19 <a17003135@alumnos.uady.mx>
2024-04-14 12:14:02 -0600

    creating blogs components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,50 @@
-import NavBar from "./components/NavBar";
- 
+import React, { useState, useEffect } from 'react';
+import NavBar from './components/NavBar';
+import BlogList from './components/BlogList';
 
 function App() {
+  const [entries, setEntries] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const blogsPerPage = 5;
+
+  useEffect(() => {
+    const fetchEntries = async () => {
+      try {
+        const response = await fetch('http://localhost:4000/entry');
+        if (response.ok) {
+          const data = await response.json();
+          setEntries(data);
+        } else {
+          throw new Error('Failed to fetch data');
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchEntries();
+  }, []);
+
+  const goToPreviousPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const goToNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
   return (
     <>
-    <NavBar />
-    <h1>Hello World!</h1>
+      <NavBar />
+      <div className="container mt-4">
+        <BlogList
+          entries={entries}
+          currentPage={currentPage}
+          blogsPerPage={blogsPerPage}
+          goToPreviousPage={goToPreviousPage}
+          goToNextPage={goToNextPage}
+        />
+      </div>
     </>
   );
 }
---
+++ frontend/src/components/CardBlog.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const CardBlog = ({ entry }) => {
+   const formattedDate = new Date(entry.created_at).toLocaleDateString('en-GB');
+   const truncatedContent = entry.content.substring(0, 70);
+
+   return (
+      <div className="card shadow-sm mb-4">
+         <div className="card-body">
+            <h5 className="card-title mb-3">{entry.title}</h5>
+            <p className="card-text text-muted">{truncatedContent}...</p>
+         </div>
+         <div className="card-footer text-muted d-flex justify-content-between align-items-center">
+            <div>
+               <p className="mb-0">Author: {entry.author}</p>
+               <p className="mb-0">Published: {formattedDate}</p>
+            </div>
+            <button className="btn btn-sm btn-primary">Read More</button>
+         </div>
+      </div>
+   );
+};
+
+export default CardBlog;
